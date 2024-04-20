9300bdcc10cbe088376b11d60d12c771bf9b296a
janggmii <janggmii17@gmail.com>
2024-04-21 00:51:57 +0700

    init

--- app/src/main/java/com/ubaya/uts_anmp_160421140/DetailFragment.kt
+++ app/src/main/java/com/ubaya/uts_anmp_160421140/DetailFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ListHobbyFragment.newInstance] factory method to
+ * Use the [DetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListHobbyFragment : Fragment() {
+class DetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class ListHobbyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_hobby, container, false)
+        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class ListHobbyFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ListHobbyFragment().apply {
+            DetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
